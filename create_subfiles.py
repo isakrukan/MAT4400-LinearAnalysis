ce28d36f51921c14f7ed77dc78c5e2d3d65fdbdc
Isak Onsager Rukan <isakonsagerrukan@eduroam-193-157-181-153.wlan.uio.no>
2024-03-06 10:12:48 +0100

    Cleaned up

--- create_subfiles.py
+++ create_subfiles.py
@@ -1,22 +1,29 @@
-import os
+import sys, os
+from contextlib import contextmanager
 
 class Stand_alone_subfiles:
 
-    def __init__(self, default_tex="TMP/_default.tex", save_to_folder="", delete_tex=False):
+    def __init__(self, default_tex="TMP/_default.tex", save_to_folder="", read_from="", delete_tex=False):
 
-        self._after_title_txt = r"\maketitle" + "\n" + r"\date{}" + "\n"
+        self._after_title_txt = r"\maketitle" + "\n" + r"\date" + "\n"
         self._end_txt = r"\end{document}"
         with open(default_tex, "r") as file:
             self._start_txt = file.readlines()
 
         self._save_to_folder = save_to_folder
         self._delete_tex = default_tex
-
-    def create_stand_alone_texes(self):
+        if read_from != "":
+            read_from = read_from + "/"
+        self._read_from = read_from
         self._subfile_names = []
-        for i in range(1,29):
+
+    def create_stand_alone_texes(self, sandwich=["Ch_","_subfile.tex"], iterable=range(1,29)):
+        for i in iterable:
+            msg = ""
             try:
                 filename_read = f"Ch_{i}_subfile.tex"
+                filename_read = self._read_from + sandwich[0] + str(i) + sandwich[1]
+                
                 with open(filename_read, "r") as file:
                     txt = file.readlines()
                 
@@ -27,6 +34,7 @@ class Stand_alone_subfiles:
                     j += 1
                             
                 filename_write = f"Ch_{i}_{title}.tex"
+                filename_write = sandwich[0] + str(i) + f"_{title}.tex"
 
                 filename_write = filename_write.replace(r"\(", ""); filename_write = filename_write.replace(r"\)", "")
                 filename_write = filename_write.replace(r"$", ""); filename_write = filename_write.replace(" ", "_")
@@ -34,23 +42,28 @@ class Stand_alone_subfiles:
                 
                 msg = f"Created {filename_write} successfully"; to_save = filename_write
                 if self._save_to_folder != "":
-                    filename_write = self._save_to_folder + "/" + filename_write
+                    filename_write = filename_write
                     msg = msg + f"at {self._save_to_folder}"
 
                 with open(filename_write, "w") as file:
                     for j in range(len(self._start_txt)):
                         file.write(self._start_txt[j])
                     
-                    middle_text = r"\setcounter{section}{" + f"{i}" + "}\n"
-                    middle_text += r"\setcounter{theorem}{" + f"{i}" + "}\n"
-                    middle_text += r"\begin{document}" + "\n"
+                    middle_text = r"\begin{document}" + "\n"
+                    try:
+                        int(i)
+                        middle_text += r"\setcounter{section}{" + f"{i}" + "}\n"
+                        middle_text += r"\setcounter{theorem}{" + f"{i}" + "}\n"
+                    except:
+                        middle_text = ""
                     
                     file.write(middle_text)
                     file.write(r"\title{" + title + "}\n")
+                    file.write(r"\author{Morten Tryti Berg and Isak Cecil Onsager Rukan. }" + "\n")
                     file.write(self._after_title_txt)
                     for j in range(1, len(txt)):
                         file.write(txt[j])
-                    file.write(self._end_txt)
+                    file.write("\n" + self._end_txt)
 
                 self._subfile_names.append(to_save)
 
@@ -65,13 +78,14 @@ class Stand_alone_subfiles:
             move = True
 
         for i in range(len(self._subfile_names)):
-            os.system("pdflatex " + location + f'"{self._subfile_names[i]}"')
-            os.system("pdflatex " + location + f'"{self._subfile_names[i]}"')
+            os.system("pdflatex " + f'"{self._subfile_names[i]}"')
+            os.system("pdflatex " + f'"{self._subfile_names[i]}"')
+
             if move:
                 os.system("mv " +  f'"{self._subfile_names[i][0:-3]}pdf" ' + location + f'"{self._subfile_names[i][0:-3]}pdf"')
             
             if self._delete_tex:
-                os.system("rm " + location + f'"{self._subfile_names[i]}"')
+                os.system("rm " f'"{self._subfile_names[i]}"')
                 
     def delete_auxiliary_fields(self):
           with open(".gitignore", "r") as file:
@@ -80,8 +94,23 @@ class Stand_alone_subfiles:
                     delete_this = line[1:].strip()
                     os.system(f"rm *{delete_this}")
 
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
+
 if __name__ == "__main__":
-    kjoor = Stand_alone_subfiles(save_to_folder="Chapters", delete_tex=True)
+    kjoor = Stand_alone_subfiles(save_to_folder="PDFs", read_from="Chapters_Files", delete_tex=True)
     kjoor.create_stand_alone_texes()
+    kjoor._read_from = "Appendix/"
+    # kjoor.create_stand_alone_texes(sandwich=["Appendix_", "_subfile.tex"], iterable=["H"])
+    kjoor.create_stand_alone_texes(sandwich=["Appendix_", "_subfile.tex"], iterable=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", 
+                                                                  "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
     kjoor.create_pdfs()
     kjoor.delete_auxiliary_fields()
